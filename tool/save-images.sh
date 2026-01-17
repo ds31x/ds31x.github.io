fd0adddc50e09d8f2bdbb38393d4a3162242c014
dsaint31 <dsaint31@gmail.com>
2026-01-17 21:12:34 +0900

    change save-images.sh

--- tool/save-images.sh
+++ tool/save-images.sh
@@ -22,7 +22,8 @@ for CHANGED_FILE in $CHANGE_LIST; do
     # URI_LIST=`ag "https://pbs.twimg.com/media/.*?(png|jpg|gif|mp4)" -o $CHANGED_FILE`
 
     # URI_LIST=`ag "https://((user-images\.githubuser.*?\/$NUM\/)|(pbs.twimg.com/media/)|(video.twimg.com/.+_video/)).*?(png|jpg|gif|mp4)" -o $CHANGED_FILE`
-    URI_LIST=$(ag -o "https://((private-user-images\.githubusercontent\.com/[0-9]+/)|(pbs\.twimg\.com/media/)|(video\.twimg\.com/.+_video/)).*?\.(png|jpe?g|gif|mp4)(\?[^\"[:space:]]*)?" "$CHANGED_FILE")
+
+    URI_LIST=$(ag -o "https://github\.com/user-attachments/assets/[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}" "$CHANGED_FILE")
     for URI in $URI_LIST; do
         FILE_NAME=`echo $URI | sed 's,^.*/,,'`
         RESOLVE_FILE_PATH="$TARGET_PATH/$FILE_NAME"
